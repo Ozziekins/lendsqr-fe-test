3c66e4670b298a6250259fc27e54554945221642
Ozziekins <o.okonicha@innopolis.university>
2024-07-20 21:41:59 +0300

    tried a new update user netlify function

--- netlify/functions/updateUser.js
+++ netlify/functions/updateUser.js
@@ -1,29 +1,41 @@
 const fs = require('fs');
 const path = require('path');
+const dataPath = path.join(__dirname, '../../db.json');
 
 exports.handler = async (event) => {
   const { id } = event.queryStringParameters;
-  const inputBody = JSON.parse(event.body);
+  const updatedData = JSON.parse(event.body);
   
-  const rawData = fs.readFileSync('../../db.json');
-  const data = JSON.parse(rawData);
-  const userIndex = data.users.findIndex(user => user.id === parseInt(id));
+  try {
+    const data = JSON.parse(fs.readFileSync(dataPath));
+    const userIndex = data.users.findIndex(user => user.id === parseInt(id));
 
-  if (userIndex === -1) {
+    if (userIndex === -1) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "User not found" })
+      };
+    }
+
+    data.users[userIndex] = {...data.users[userIndex], ...updatedData};
+
+    fs.writeFileSync(dataPath, JSON.stringify(data));
+
     return {
-      statusCode: 404,
-      body: JSON.stringify({ message: "User not found" })
+      statusCode: 200,
+      body: JSON.stringify(data.users[userIndex]),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+  } catch (error) {
+    console.error('Failed to update user:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Failed to update user status",
+        error: error.message
+      })
     };
   }
-
-  data.users[userIndex] = {...data.users[userIndex], ...inputBody};
-  fs.writeFileSync('../../db.json', JSON.stringify(data));
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data.users[userIndex]),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
 };
